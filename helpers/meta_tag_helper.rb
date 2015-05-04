9328590d07eb073c425c72eef3fb8b4769523b52
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-05-04 13:28:09 +0800

    finished meta tag helpers

--- helpers/meta_tag_helper.rb
+++ helpers/meta_tag_helper.rb
@@ -1,8 +1,7 @@
 module MetaTagHelper
-  # Sets the page title which is composed of 3 elements
-  #   1) The Site name
-  #   2) A separator
-  #   3) The title of each page
+  # Sets the page meta description and keywords which can be composed by either
+  #   a) data in config.yml (which is site-wide), or
+  #   b) data set in the FrontMatter of each page
 
   def meta_keywords
     site_keywords = data.config.site.meta_keywords
@@ -14,20 +13,34 @@ module MetaTagHelper
       keywords = page_keywords
     end
 
-    if keywords.nil? || keywords.empty?
-      raise RuntimeError, "You must provide meta keywords for your site/page!"
+    if keywords.is_a?(String)
+      # Props to @jordanmaguire for the help on this
+      keywords = keywords.split(",").map(&:strip)
     end
 
-    [*keywords].join(", ")
+    if keywords.nil? || keywords.empty?
+      raise RuntimeError, "You must provide meta keywords for your site/page!"
+    elsif keywords.count > 15
+      raise RuntimeError, "Too many meta keywords! Should be between 10-15 terms. You have: #{keywords.count}"
+    end
+
+    keywords.join(", ")
   end
 
   def meta_description
-    description = data.page.meta_description || data.config.site.meta_description
+   site_description = data.config.site.meta_description
+   page_description = data.page.meta_description
+
+    if page_description.nil? || page_description.empty?
+      description = site_description
+    else
+      description = page_description
+    end
 
     if description.nil? || description.empty?
       raise RuntimeError, "You must provide a meta description for your site/page!"
     elsif description.length > 160
-      raise RuntimeError, "Meta description too long! Should be between 150-160 characters."
+      raise RuntimeError, "Meta description too long! Should be between 150-160 characters. You have: #{description.length}"
     end
   end
 end
